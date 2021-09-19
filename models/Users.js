bf01771f221cfd6bd50769815ad9cc25b823697b
Hansaem Kim <hansaem_kim@hotmail.com>
2021-09-19 12:33:58 -0400

    Create User Model

---
+++ models/Users.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: {
+      type: String,
+      required: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    },
+
+    first_name: {
+        type: String,
+        required: true
+    },
+
+    last_name: {
+        type: String,
+        required: true
+    }}, 
+    
+    {
+    timestamps: true
+    }
+)
+
+module.exports = User = mongoose.model('User', UserSchema);
