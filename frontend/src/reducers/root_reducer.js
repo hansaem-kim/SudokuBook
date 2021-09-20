61937f2b26b7475a2148e302c416a450bbe2603a
simonpuno <simonpuno@gmail.com>
2021-09-20 14:18:33 -0500

    Add frontend actions, util, and reducer for Sudoku

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import EntitiesReducer from './entities_reducer';
 import session from './session_reducer';
 
 const RootReducer = combineReducers({
+    entities: EntitiesReducer,
     session
 });
 
