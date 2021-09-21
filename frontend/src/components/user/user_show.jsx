40a11556ab831f6a91caba5418ac2ef12e9115c3
Quinn Mullaney <skiingqm@gmail.com>
2021-09-21 19:25:59 -0400

    going to pull from main

---
+++ frontend/src/components/user/user_show.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class UserShow extends React.Component{
+    constructor(props){
+        super(props);
+        
+    }
+}
