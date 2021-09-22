cf90624348e2289434ec8ed328f5f57ef334d0e3
Aiden Chang <skylineciel@gmail.com>
2021-09-22 12:04:08 -0400

    Working on search

--- frontend/src/components/search/search_container.js
+++ frontend/src/components/search/search_container.js
@@ -1,10 +1,14 @@
 import { connect } from 'react-redux';
+import { fetchPosts }  from "../../actions/post_actions";
+
 import Search from './search';
 
 const mapStateToProps = state => ({
+    posts: Object.values(state.entities.posts)
 });
 
 const mapDispatchToProps = dispatch => ({
+    fetchPosts: () => (dispatch(fetchPosts())),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Search);
