81731e5199701956aacb466f338496d0c1e2b16c
Aiden Chang <skylineciel@gmail.com>
2021-09-22 17:23:42 -0400

    Edit home

--- frontend/src/components/main/home.js
+++ frontend/src/components/main/home.js
@@ -1,20 +1,54 @@
 import React from 'react';
 import Input from '../input/input'
+// <Input />
+import { Link } from 'react-router-dom';
+
+
 class Home extends React.Component {
     constructor(props) {
         super(props);
+
         this.handleClick = this.handleClick.bind(this);
+        this.generateRandom = this.generateRandom.bind(this);
+
+        this.props.fetchSudokus();
+        this.state = {
+            value: 0,
+            display: false
+        };
     }
 
     handleClick(e) {
         e.preventDefault();
     }
 
+    generateRandom() {
+        this.setState({
+            value: Math.floor(Math.random() * (13 + 1) ),
+            display: true
+        });
+    }
+
+    playButton() {
+        if (this.props.sudokus.data) {
+            const sudoku  = this.props.sudokus.data[this.state.value];
+            console.log(sudoku)
+
+            return (
+                <Link className="random-sudoku-link" key={sudoku._id} to={`/sudokus/${sudoku._id}`} >
+                    <button>Play</button>
+                </Link>
+            )
+        }
+    }
+
     render() {
         return (
             <div>
-                <p>home</p>
-                <Input />
+                <h1>home</h1>
+                <p>Play random sudoku game</p>
+                <button onClick={this.generateRandom}>Generate</button>
+                {this.playButton()}
             </div>
         );
     }
--- frontend/src/components/main/home_container.js
+++ frontend/src/components/main/home_container.js
@@ -1,12 +1,14 @@
 import { connect } from 'react-redux';
 import Home from './home';
+import { fetchSudokus } from '../../actions/sudoku_actions';
 
 const mapStateToProps = state => ({
-  currentUser: state.session.currentUser
+  currentUser: state.session.currentUser,
+  sudokus: state.entities.sudokus
 });
 
 const mapDispatchToProps = dispatch => ({
-
+  fetchSudokus: () => dispatch(fetchSudokus())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
