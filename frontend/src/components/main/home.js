a108bc4b124a6c1c1da95cc4942ea40cb9a01267
Aiden Chang <skylineciel@gmail.com>
2021-09-23 10:46:53 -0400

    Main page initial setup, needs to work on more

--- frontend/src/components/main/home.js
+++ frontend/src/components/main/home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Input from '../input/input'
 // <Input />
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+
 
 
 class Home extends React.Component {
@@ -9,7 +10,6 @@ class Home extends React.Component {
         super(props);
 
         this.handleClick = this.handleClick.bind(this);
-        this.generateRandom = this.generateRandom.bind(this);
 
         this.props.fetchSudokus();
         this.state = {
@@ -18,40 +18,30 @@ class Home extends React.Component {
         };
     }
 
-    handleClick(e) {
-        e.preventDefault();
-    }
-
-    generateRandom() {
+    handleClick() {
         this.setState({
             value: Math.floor(Math.random() * (13 + 1) ),
             display: true
         });
-    }
-
-    playButton() {
         if (this.props.sudokus.data) {
             const sudoku  = this.props.sudokus.data[this.state.value];
             console.log(sudoku)
-
-            return (
-                <Link className="random-sudoku-link" key={sudoku._id} to={`/sudokus/${sudoku._id}`} >
-                    <button>Play</button>
-                </Link>
-            )
+            this.props.history.push(`/sudokus/${sudoku._id}`);
         }
     }
 
     render() {
         return (
-            <div>
-                <h1>home</h1>
-                <p>Play random sudoku game</p>
-                <button onClick={this.generateRandom}>Generate</button>
-                {this.playButton()}
+            <div className="home-page-wrapper">
+                <div className="home-page-content">
+                    <h1>Ready to play sudoku?</h1>
+                    <p>If you are ready, challenge it 
+                        <button onClick={this.handleClick}>Right Now!</button>
+                    </p>
+                </div>
             </div>
         );
     }
 }
 
-export default Home;
+export default withRouter(Home);
