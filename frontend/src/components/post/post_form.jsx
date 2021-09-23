ecc3f84d494f37a9610066b98d6ab18803d1bf20
Hansaem Kim <hansaem_kim@hotmail.com>
2021-09-23 11:38:24 -0400

    Fix Edit Post Bug

--- frontend/src/components/post/post_form.jsx
+++ frontend/src/components/post/post_form.jsx
@@ -24,11 +24,11 @@ class PostForm extends React.Component{
         this.setState({time: this.props.time, puzzle: this.props.sudokuId}, () => 
         this.props.action(this.state)
             .then(this.props.closeModal)
-            .then(()=> this.props.updateSudoku(
+            .then(()=> {if (this.props.time) this.props.updateSudoku(
                 {"time": { minutes: this.props.time.minutes, seconds: this.props.time.seconds },
                  "user": this.props.currentUser,
-                "id":this.props.sudokuId})
-            .then(()=> this.props.history.push('/feed')))
+                "id":this.props.sudokuId})})
+            .then(()=> this.props.history.push('/feed'))
         );
     }
 
