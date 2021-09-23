773168187ae79a07478b23d814968b2ba1a7830e
Quinn Mullaney <skiingqm@gmail.com>
2021-09-23 08:34:47 -0400

    profile posts

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import PostIndexItem from '../post/post_index_item';
 
 class Profile extends React.Component {
     constructor(props) {
         super(props);
 
     }
+
+    componentDidMount(){
+      this.props.fetchPosts();
+    }
     
     render() {
+      const {posts, deletePost, currentUser} = this.props;
           return (
             <div>
               <h2>{this.props.currentUser.username}'s profile</h2>
+              <div>
+                {posts.map(post => {
+                  if (post.user == currentUser.id){
+                    return <PostIndexItem post={post} currentUser={currentUser}
+                    deletePost={deletePost} key={post._id} />
+                  }
+                })}
+              </div>
             </div>
           );
     }
--- frontend/src/components/profile/profile_container.js
+++ frontend/src/components/profile/profile_container.js
@@ -1,12 +1,18 @@
 import { connect } from 'react-redux';
 import Profile from './profile';
+import { fetchPosts, deletePost } from "../../actions/post_actions";
+import { closeModal } from "../../actions/modal_actions";
 
 const mapStateToProps = state => ({
-    currentUser: state.session.user
+    currentUser: state.session.user,
+    posts: Object.values(state.entities.posts).reverse()
 });
 
 const mapDispatchToProps = dispatch => {
   return {
+    fetchPosts: () => (dispatch(fetchPosts())),
+    deletePost: (postId) => (dispatch(deletePost(postId))),
+    closeModal: () => dispatch(closeModal()),
   };
 };
 
