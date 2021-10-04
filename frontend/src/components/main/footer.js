c0d5f98cc8c6f50abf43f6af064caa53428497f8
Aiden Chang <skylineciel@gmail.com>
2021-10-04 11:58:28 -0400

    Add instruction in footer

--- frontend/src/components/main/footer.js
+++ frontend/src/components/main/footer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = props => {
     if (!props.loggedIn) return null;
@@ -7,6 +9,8 @@ const Footer = props => {
         <div className="footer">
             <div className="footer-content">
                 <div className="footer-content-links">
+                    <FontAwesomeIcon icon={faQuestionCircle} onClick={() => props.openModal('instructions')}/>
+
                     <a href="https://github.com/hansaem-kim/SudokuBook" target="_blank">
                         <i className="fab fa-github"></i>
                     </a>
